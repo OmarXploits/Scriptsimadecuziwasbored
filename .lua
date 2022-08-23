da396d79677b68f6e7656c6fd5f0f9a75d19bc65
OmarXploits <106861748+OmarXploits@users.noreply.github.com>
2022-08-23 15:26:10 +0200

    Create .lua

---
+++ .lua
@@ -0,0 +1,22 @@
+--This script is free to use, do anything with it. i will not obfuscate anything here. It would still be cool if you add credits.
+--[[
+  ____        
+ | __ ) _   _ 
+ |  _ \| | | |
+ | |_) | |_| |
+ |____/ \__, |
+        |___/ 
+    __                     __  __      _       _ _       
+  / _ \ _ __ ___   __ _ _ _\ \/ /_ __ | | ___ (_) |_ ___ 
+ | | | | '_ ` _ \ / _` | '__\  /| '_ \| |/ _ \| | __/ __|
+ | |_| | | | | | | (_| | |  /  \| |_) | | (_) | | |_\__ \
+  \___/|_| |_| |_|\__,_|_| /_/\_\ .__/|_|\___/|_|\__|___/
+                                |_|                      
+]]--
+                                
+
+_G.printtoolnames = true     --If you do not know the tool's names that you have, set this to true. (CHECK THE CONSOLE!)
+_G.noclip = true             --If you want your character to be nocliped, set this to true. (WORKS BETTER IF ITS SET TO TRUE!)
+local target = "alex_omar41" --Set your targets name. (FULL NAME ELSE IT WILL NOT WORK!) 
+local tool = "BoomBox"       --Put the Name of the tool you want to kill with.
+
